050f98f74dd7bb70b834a49d8fa87c09276d382d
Geraldine <hello@geraldiner.com>
2021-09-06 21:28:14 -1000

    force routes for login

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 const homeController = require("../controllers/home");
 const authController = require("../controllers/auth");
 
 // @desc  Landing page
 // @route GET /
-router.get("/", homeController.getIndex);
+router.get("/", ensureGuest, homeController.getIndex);
 
 // @desc  Login form
 // @route GET /login
